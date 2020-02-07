2aee0f11e74362174918cbe2e0a7d641036fb301
Alumno <Alumno@PC-PC>
2020-02-07 10:01:39 -0300

    primer acercamiento al codigo

---
+++ src/main/java/com/mycompany/zoologicotangamandapio/Zoologico.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.zoologicotangamandapio;
+import java.util.ArrayList;
+/**
+ *
+ * @author Alumno
+ */
+public class Zoologico {
+    ArrayList<Animal> lista = new ArrayList<Animal>();
+    
+}
